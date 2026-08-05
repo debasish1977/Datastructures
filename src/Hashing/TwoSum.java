2be381fbbdb8ef97916a0d7d3ba4b334e0b21950
DEBASISH CHAKRABORTY <31114049+debasish1977@users.noreply.github.com>
2026-08-04 22:59:52 -0400

    Update TwoSum.java - improve clarifying questions and code formatting

--- src/Hashing/TwoSum.java
+++ src/Hashing/TwoSum.java
@@ -1,31 +1,29 @@
 package Hashing;
-
 //Clarifying Questions
-//Is exactly one valid pair guranteed?
-//Should I return indices or values?
-//Can the same array element be used twice?
-//Can the input contain duplicate values?
+//Can the array contain positive, negative & duplicate values?
+//Is exactly one valid solution guranteed?
+//Can the same index be used more than once?
+//Should I return the pair of indices in any order?
 
 //Approach
-//I will solve this using HashMap & by calculating complements
+//I will solve this HashMap and by calculating complements
 
 //Time Complexity: O(N)
 //Space Complexity: O(N)
 
 import java.util.HashMap;
-
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         if(nums==null || nums.length < 2) return new int[0];
-        int complement = 0;
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int complement = 0;
         for(int i=0;i<nums.length;i++){
             complement = target - nums[i];
             if(map.containsKey(complement)){
-                return new int[]{map.get(complement),i};
+                return new int[] {map.get(complement),i};
             }
             map.put(nums[i],i);
         }
-        return null;
+        return new int[0];
     }
 }
